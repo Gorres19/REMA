f6bed1d91b73092a710452f57de22a6ed8db9410
gorres19 <gorresjoshua15@gmail.com>
2016-05-06 10:52:20 +0800

    Login and Registration committed

---
+++ app/src/main/java/com/android/rema3/AppControl/AppConfig.java
@@ -0,0 +1,11 @@
+package com.android.rema3.AppControl;
+
+/**
+ * Created by User on 4/25/2016.
+ */
+public class AppConfig {
+    public static String URL_LOGIN = "http://192.168.1.3/android_login_api/Login.php";
+    public static String URL_REGISTER = "http://192.168.1.3/android_login_api/Register.php";
+}
+
+
